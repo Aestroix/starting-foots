7480acfb8fbb8a2e8c2ea8bc1b810b39a717cc4b
Aestroix <aestroixxes@gmail.com>
2020-09-22 02:54:46 +0530

    test1

--- hello_world.py
+++ hello_world.py
@@ -2,10 +2,13 @@
 
 BEFORE COMMITING THERE IS A STAGING AREA 
 SO WHEN YOU MAKE A CHANGE YOU CAN ADD THAT FILE TO THAT STAGING AREA
-'''
-'''
 
 git add <file> to add file to staging area
 git rm --cached <file> to unstage
+
+To commit make sure that you configure the git using your email and username
+
+git config --global user.email "email"
+git config --global user.name "username on git"
 '''
 print('hello world')
