9596a8248faa9efc0cf28211e254b301bd6834d9
Aestroix <aestroixxes@gmail.com>
2020-09-22 03:55:50 +0530

    saved last

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,5 @@
 '''
+THERE IS ONE MASTER BRANCH
 
 BEFORE COMMITING THERE IS A STAGING AREA 
 SO WHEN YOU MAKE A CHANGE YOU CAN ADD THAT FILE TO THAT STAGING AREA
@@ -10,5 +11,11 @@ To commit make sure that you configure the git using your email and username
 
 git config --global user.email "email"
 git config --global user.name "username on git"
+
+when you commit something the commit head goes above the latest added file
+
+git branch <branch_name>: to add a branch
+git checkout <branch_name>: to change branch
+git merge <branch_name>: to merge a branch to current branch
 '''
 print('hello world')
