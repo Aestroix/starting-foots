fb97893d0659f200839c85f89df63885a9e338dd
Aestroix <aestroixxes@gmail.com>
2020-09-22 02:51:57 +0530

    test

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+'''
+
+BEFORE COMMITING THERE IS A STAGING AREA 
+SO WHEN YOU MAKE A CHANGE YOU CAN ADD THAT FILE TO THAT STAGING AREA
+'''
+'''
+
+git add <file> to add file to staging area
+git rm --cached <file> to unstage
+'''
+print('hello world')
